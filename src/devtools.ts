e3aa496fa0991294e177c86d096a2319def45bfd
ryandcolesr032777-spec <ryandcolesr032777@gmail.com>
2025-07-27 16:16:55 -0700

    Delete src directory

--- src/devtools.ts
+++
@@ -1,5 +0,0 @@
-chrome.devtools.panels.create(
-  "Snippets",
-  "", // We don't provide an image
-  "../panel.html"
-);
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends (infer U)[]
-    ? DeepPartial<U>[]
-    : T[P] extends Readonly<infer U>[]
-    ? Readonly<DeepPartial<U>>[]
-    : DeepPartial<T[P]>;
-};
